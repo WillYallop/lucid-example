98a9ee6ea22dbc11b150302d7f23c77eb73eab6c
WillYallop <hello@williamyallop.com>
2022-05-01 22:32:27 +0100

    Config changes

--- lucid.config.js
+++ lucid.config.js
@@ -1,5 +1,16 @@
 module.exports = {
     build: './dist',
+    app_urL: 'http://192.168.1.153:7343',
+    key: process.env.KEY,
+    storage: {
+        location: 'aws', // or 'local'
+        // optional
+        aws: {
+            bucket: process.env.S3_BUCKET_NAME,
+            access_key_id: process.env.S3_ACCESS_KEY_ID,
+            secret_access_key: process.env.S3_SECRET_ACCESS_KEY
+        }
+    },
     allowOrigins: [
         "*"
     ],
